f5bae8660dbc7adde90473c219a5f0bb921bd0ac
Muhammad Aero Dinamika Oryan <aerodinamika3@gmail.com>
2025-05-19 23:59:27 +0700

    Add files via upload

--- market_research_server.py
+++ market_research_server.py
@@ -1,5 +1,5 @@
-
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -52,4 +52,5 @@ def map_intuition_to_action():
     return jsonify({"status": "success", "message": "Action mapped based on intuition"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
